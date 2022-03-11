3798d4c036d734d97dd37ec0eacc8307cc6f2ad8
hongdtph14095 <77144191+hongdtph14095@users.noreply.github.com>
2022-03-11 09:33:51 +0700

    first commit

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+const a = 10;
+const b = 20;
+const result = sum(a, b);
+console.log(result);
---
+++ src/app.ts
@@ -0,0 +1,34 @@
+// function sum(a: number, b: number):number {
+//     return a + b;
+// }
+// const a: number = 10;
+// const b: number = 20;
+// const result = sum(a,b);
+// console.log(result);
+type TPerson={
+    id: number;
+    name: string;
+    age: number;
+    status?:boolean
+}
+
+const person:TPerson ={
+    id:1,
+    name: "Linh",
+    age:24
+}
+
+const persons:TPerson[]= [
+    {id:1, name:"Linh",age:24, status:true},
+    {id:2, name:"Dun",age:20, status:false},
+]
+console.log(person.name)
+
+
+type AppProps={
+    message:string
+}
+const DemoApp=(props: AppProps) =>{
+    console.log(props)
+}
+// DemoApp("heloo linh nho");
